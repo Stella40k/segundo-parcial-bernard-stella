e7d1e1fa79fff024054eade4ece5418ae7d112d4
Bernard <bernardstella0704@gmail.com>
2025-06-27 11:49:45 -0300

    error en la estructura, se comento todo para cambiar lo de model

--- src/models/movie.model.js
+++ src/models/movie.model.js
@@ -1,12 +1,17 @@
-import mysql from "mysql2";
+//?import { Sequelize } from "";
 
-async function main() {
+//?async function main() {
+  //?  const connection = mysql.createConnection({
+  //?      DB_NAME: "movies",
+     //?   DB_USER: "root",
+    //?    DB_PASSWORD: "",
+    //?    DB_HOST: "localhost",
+    //?    DB_DIALECT: "mysql",
+//?    });    
+//?};
 
-const connection = await mysql.createConnection({
-    DB_NAME: "movies",
-    DB_USER: "root",
-    DB_PASSWORD: "",
-    DB_HOST: "localhost",
-    DB_DIALECT: "mysql",
-});    
-};
+//?export = connection()
+
+
+//aca hay q validar los tipos de datos de las peliculas no 
+//la configuracion de la bd ARREGLAR
