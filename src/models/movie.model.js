d9b6a509e9fa1e66013840536181924db0088289
Bernard <bernardstella0704@gmail.com>
2025-06-27 13:01:31 -0300

    estructura de movie

--- src/models/movie.model.js
+++ src/models/movie.model.js
@@ -1,6 +1,28 @@
 import { Sequelize } from "sequelize";
+const sequelize = new Sequelize()
 
-//creacion
-//llamar a todos
-//llamar uno
-//borrar
+const Movie = sequelize.define("movie", {
+      id: {
+        type: sequelize.INTEGER,
+        autoIncrement: true,
+        allowNull: false
+      },
+     title:{
+        DataTypes.STRING(35)
+
+     },
+     director:{
+        DataTypes.TEXT
+     },
+     duration:{
+         DataTypes.TEXT
+     },
+     genre:{
+        DataTypes.TEXT
+     },
+     description: {
+
+     }
+})
+
+export default Character;
